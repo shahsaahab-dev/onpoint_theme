f9fffa5ffc7d8142fbc31ecfabe28e2b3c3345b6
shahsaahab-dev <naqishah2997@gmail.com>
2020-01-16 08:47:36 -0800

    Demo Preview Added OCDI

--- theme-functions/demo-import.php
+++ theme-functions/demo-import.php
@@ -6,9 +6,10 @@ function onpoint_demo_import(){
         array(
             'import_file_name' => 'Demo Import 1',
             'categories' => array('New Category','Old Category'),
-            'local_import_file' => trailingslashit(get_template_directory()) . '/demo-import/onpoint-local.xml',
+            'local_import_file' => trailingslashit(get_template_directory()) . '/demo-import/onpoint.xml',
             'local_import_widget_file' => trailingslashit(get_template_directory()) . '/demo-import/onpoint-widgets.wie',
             'local_import_customizer_file' => trailingslashit(get_template_directory()) . '/demo-import/onpoint-customizer.dat',
+            'import_preview_image_url' => trailingslashit(get_template_directory()) . 'screenshot.png',
             'import_notice' => __(' This is the Demo 1 of Onpoint, More Coming Soon...', 'onpoint_theme'),
         ),
     );
